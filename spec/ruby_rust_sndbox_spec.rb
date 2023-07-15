72a9dc1e63582d75048c4f86226d68f734d94fd4
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2023-07-16 01:52:53 +0300

    [feat]: remove test

--- spec/ruby_rust_sndbox_spec.rb
+++ spec/ruby_rust_sndbox_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RubyRustSndbox do
   it "has a version number" do
     expect(RubyRustSndbox::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
